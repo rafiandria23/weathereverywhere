852cdd9f45cfde6fed4c201d9d4593925501e842
Adam Rafiandri <rafiandria23@gmail.com>
2022-02-21 14:54:21 +0700

    Initial Commit

---
+++ Shared/WeatherEverywhereApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherEverywhereApp.swift
+//  Shared
+//
+//  Created by Adam Rafiandri on 02/21/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherEverywhereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
